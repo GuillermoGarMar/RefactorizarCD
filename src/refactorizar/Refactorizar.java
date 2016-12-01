323216db72fa810e4c1b03d1b55ae21c7463702e
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-12-01 09:07:35 +0100

    c2

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -30,57 +30,56 @@ public class Refactorizar {
         }
         return n;
     }
-    
-    public static int calculaDigitos(int numero){
-        int cont=0;
+
+    public static int calculaDigitos(int numero) {
+        int cont = 0;
         while (numero != 0) {
-                numero = numero / 10;
-                cont++;
-            }
+            numero = numero / 10;
+            cont++;
+        }
         return cont;
     }
-    
+
     public static boolean Primo = false;
-    
-    public static void MostrarPrimos(int num1, int num2, int cont){
+
+    public static void MostrarPrimos(int num1, int num2, int cont) {
         if (num1 == num2) {
-                if (cont < 4) {
-                    Primo = true;
+            if (cont < 4) {
+                Primo = true;
+            } else {
+                if (cont % 2 == 0) {
+                    Primo = false;
                 } else {
-                    if (cont % 2 == 0) {
-                        Primo = false;
-                    } else {
-                        int contador1 = 0;
-                        int i1 = 1;
-                        int limite = (cont - 1) / 2;
-                        if (limite % 2 == 0) {
-                            limite--;
-                        }
+                    int contador1 = 0;
+                    int i1 = 1;
+                    int limite = (cont - 1) / 2;
+                    if (limite % 2 == 0) {
+                        limite--;
+                    }
 
-                        while (i1 <= limite) {
-                            if (cont % i1 == 0) {
-                                contador1++;
-                            }
-                            i1 += 2;
-                            if (contador1 == 2) {
-                                i1 = limite + 1;
-                            }
+                    while (i1 <= limite) {
+                        if (cont % i1 == 0) {
+                            contador1++;
                         }
-
-                        if (contador1 == 1) {
-                            Primo = true;
+                        i1 += 2;
+                        if (contador1 == 2) {
+                            i1 = limite + 1;
                         }
                     }
-                }
 
-                if (Primo == true) {
-                    System.out.println(cont);
+                    if (contador1 == 1) {
+                        Primo = true;
+                    }
                 }
             }
 
+            if (Primo == true) {
+                System.out.println(cont);
+            }
+        }
+
     }
 
-   
     public static void main(String arg[]) {
         int numDigitos = 0;
         int ndigitos = 0;
@@ -90,8 +89,8 @@ public class Refactorizar {
         for (int i = 1; i <= 99999; i++) {
 
             ndigitos = calculaDigitos(i);
-            MostrarPrimos(ndigitos,numDigitos,i);
-            
+            MostrarPrimos(ndigitos, numDigitos, i);
+
         }
     }
 
